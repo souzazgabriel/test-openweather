1babaf59fd9c16978644c00bf2557139b3c372ac
souzazgabriel <rodriguessouzagabriel6@gmail.com>
2025-09-22 12:51:43 -0300

    Melhoria na eficiencia
    Removido o import os no config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,5 @@
-import os
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql://airflow:airflow@mysql:3306/weather_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    API_KEY = os.getenv('OPENWEATHER_API_KEY', '4ca56aa5b0eec444c9fee770b077517f')
+    API_KEY = '4ca56aa5b0eec444c9fee770b077517f'
     BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
